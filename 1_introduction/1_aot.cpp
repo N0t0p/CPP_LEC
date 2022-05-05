18b71195e05879ac3467e828f7a7a926b8528050
N0t0p <sairoj25@gmail.com>
2022-05-05 13:27:30 +0545

    Area of triangle

---
+++ 1_introduction/1_aot.cpp
@@ -0,0 +1,15 @@
+// q. WAP TO CALCULATE THE AREA OF TRIANGLE 
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int base,height,area ;
+    cout<<("Enter the value of height and base :")
+    cin>>height;
+    cin>>basic;
+    area = 1/2*(base*height);
+    cout<<("The area of triangle is "<<area);
+    return 0 ;
+
+
+}
