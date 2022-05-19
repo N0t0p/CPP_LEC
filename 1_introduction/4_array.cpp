6a2a382ae1edf74588ff5aafa987bf180d21b3d5
N0t0p <sairoj25@gmail.com>
2022-05-19 14:09:38 +0545

    array changes

--- 1_introduction/4_array.cpp
+++ 1_introduction/4_array.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main (){
     int array[5] = {1,2,3,4,5};
-  std::cout <<"The array of index 0 is " <<array[0] <<endl;
-  std::cout <<"The array of index 1 is " <<array[1] <<endl;
-  std::cout <<"The array of index 2 is " <<array[2] <<endl;
-  std::cout <<"The array of index 3 is " <<array[3] <<endl;
-  std::cout <<"The array of index 4 is " <<array[4] <<endl;
+  cout <<"The array of index 0 is " <<array[0] <<endl;
+  cout <<"The array of index 1 is " <<array[1] <<endl;
+  cout <<"The array of index 2 is " <<array[2] <<endl;
+  cout <<"The array of index 3 is " <<array[3] <<endl;
+  cout <<"The array of index 4 is " <<array[4] <<endl;
     return 0;
 }
 
