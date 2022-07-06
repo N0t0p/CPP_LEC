2a60b054626e6a8bc189737280d3abab7d2b0ae2
N0t0p <sairoj25@gmail.com>
2022-07-06 12:40:10 +0545

    Lab 5 Polymorphism

--- Lab/lab_sheet_5/4_Area.cpp
+++ Lab/lab_sheet_5/4_Area.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<math.h>
 using namespace std ;
-
+class area {
+    public :
 void calculateArea (float a,float b,float c){
     float s , area ;
     s = (a+b+c)/2;
@@ -16,9 +17,11 @@ void calculateArea (float length , float breadth)
 {
     cout<<"The area of rectangle is "<<length*breadth<<endl;
 }
+};
 int main ()
-{
-    calculateArea(5,6,7);
-    calculateArea(5,6);
-    calculateArea(5);
+{   
+    area a;
+   a.calculateArea(5,6,7);
+   a.calculateArea(5,6);
+   a.calculateArea(5);
 }
